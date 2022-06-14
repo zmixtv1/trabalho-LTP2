429018c424aa0c0259bc4b5942cd585c8e73fb72
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2022-06-14 20:51:40 -0300

    atualização

--- pizzariaDelicious.py
+++ pizzariaDelicious.py
@@ -6,19 +6,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   banco = bd.SQL('root','','projeto_final')
-   comando = "SELECT idt_pizza,nme_pizza,dsc_pizza,vlr_pizza FROM tb_pizza;"
+   banco = bd.SQL('root','uniceub','projeto_final')
+   comando = "SELECT * FROM tb_pizza;"
    cs = banco.consultar(comando, [])
    dados = ""
-   for (idt, nme, dsc, vlr) in cs:
+   for (idt, nme, dsc, vlr, img) in cs:
        dados += '''
                    <h3>{}</h3>
                    <div>
+                       <img src="{}" width="140px"alt=""> <br>
                        <p>IDT Pizza: {}</p>
                        <p>Descreição da Pizza: {}</p>
                        <p>valor da Pizza: {}</p>
                    </div>
-                '''.format(nme, idt, dsc, vlr)
+                '''.format(nme, img, idt, dsc, vlr)
    return render_template('home.html', dados = dados)
 
 
